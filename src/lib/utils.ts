67f61bb8ada62e2e082eddee06fe66fc9cac0bf3
cyberpompier <cyberpompier@gmail.com>
2025-08-14 19:02:56 +0200

    Reverted all changes back to version 9dde399bd3969b9fc3fdd40601b1e508d94950c8

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,12 +1,6 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
-}
-
-export function getInitials(nom: string, prenom: string): string {
-  const nomInitial = nom ? nom.charAt(0).toUpperCase() : '';
-  const prenomInitial = prenom ? prenom.charAt(0).toUpperCase() : '';
-  return `${prenomInitial}${nomInitial}`;
+  return twMerge(clsx(inputs));
 }
