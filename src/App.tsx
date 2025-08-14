da875e0dc718fd46636f5e619d286ec2131ed276
cyberpompier <cyberpompier@gmail.com>
2025-08-14 12:41:45 +0200

    [dyad] Correction des liens de menu non fonctionnels - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,9 @@ import PersonnelForm from './pages/PersonnelForm';
 import EquipementForm from './pages/EquipementForm';
 import PersonnelEquipements from './pages/PersonnelEquipements';
 import EquipementDetail from './pages/EquipementDetail';
+import Reports from './pages/Reports';
+import Notifications from './pages/Notifications';
+import Profile from './pages/Profile';
 
 function App() {
   return (
@@ -20,6 +23,7 @@ function App() {
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
+          <Route path="/dashboard" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
           <Route path="/personnel" element={<PrivateRoute><Personnel /></PrivateRoute>} />
           <Route path="/personnel/nouveau" element={<PrivateRoute><PersonnelForm /></PrivateRoute>} />
           <Route path="/personnel/:id" element={<PrivateRoute><PersonnelDetail /></PrivateRoute>} />
@@ -29,6 +33,9 @@ function App() {
           <Route path="/equipements/:id" element={<PrivateRoute><EquipementDetail /></PrivateRoute>} />
           <Route path="/controles" element={<PrivateRoute><Controles /></PrivateRoute>} />
           <Route path="/parametres" element={<PrivateRoute><Settings /></PrivateRoute>} />
+          <Route path="/reports" element={<PrivateRoute><Reports /></PrivateRoute>} />
+          <Route path="/notifications" element={<PrivateRoute><Notifications /></PrivateRoute>} />
+          <Route path="/profile" element={<PrivateRoute><Profile /></PrivateRoute>} />
         </Routes>
       </SessionProvider>
     </Router>
