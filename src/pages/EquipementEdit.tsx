84759308f91daed03f40762a46b5665b0dd91de8
cyberpompier <cyberpompier@gmail.com>
2025-08-15 14:31:08 +0200

    Updated src/pages/EquipementEdit.tsx

--- src/pages/EquipementEdit.tsx
+++ src/pages/EquipementEdit.tsx
@@ -171,7 +171,7 @@ export default function EquipementEdit() {
                           </SelectTrigger>
                         </FormControl>
                         <SelectContent>
-                          <SelectItem value="Casque F1">🪖 Casque F1</SelectItem>
+                          <SelectItem value="casque">🪖 Casque</SelectItem>
                           <SelectItem value="veste">🧥 Veste</SelectItem>
                           <SelectItem value="surpantalon">👖 Surpantalon</SelectItem>
                           <SelectItem value="gants">🧤 Gants</SelectItem>
