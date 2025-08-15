0c7cd20a12b9e1ee4e7db26d88d4a3f3536c4314
cyberpompier <cyberpompier@gmail.com>
2025-08-15 12:56:39 +0200

    [dyad] wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,20 @@ import Equipements from '@/pages/Equipements';
 import ControleForm from '@/pages/ControleForm';
 import NotFound from '@/pages/NotFound';
 import { Toaster } from 'react-hot-toast';
+import Dashboard from '@/pages/Dashboard';
+import ControlesPage from '@/pages/Controles';
+import ControleDetail from '@/pages/ControleDetail';
+import ControleEdit from '@/pages/ControleEdit';
+import EquipementDetail from '@/pages/EquipementDetail';
+import EquipementForm from '@/pages/EquipementForm';
+import Personnel from '@/pages/Personnel';
+import PersonnelDetail from '@/pages/PersonnelDetail';
+import PersonnelForm from '@/pages/PersonnelForm';
+import PersonnelEquipements from '@/pages/PersonnelEquipements';
+import Habillement from '@/pages/Habillement';
+import Notifications from '@/pages/Notifications';
+import Rapports from '@/pages/Rapports';
+import Settings from '@/pages/Settings';
 
 export default function App() {
   return (
@@ -16,6 +30,11 @@ export default function App() {
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/dashboard" element={
+            <PrivateRoute>
+              <Dashboard />
+            </PrivateRoute>
+          } />
           <Route path="/profile" element={
             <PrivateRoute>
               <Profile />
@@ -26,9 +45,74 @@ export default function App() {
               <Equipements />
             </PrivateRoute>
           } />
+          <Route path="/equipements/nouveau" element={
+            <PrivateRoute>
+              <EquipementForm />
+            </PrivateRoute>
+          } />
+          <Route path="/equipements/:id" element={
+            <PrivateRoute>
+              <EquipementDetail />
+            </PrivateRoute>
+          } />
           <Route path="/controle/:id" element={
             <PrivateRoute>
               <ControleForm />
+            </PrivateRoute>
+          } />
+          <Route path="/controles" element={
+            <PrivateRoute>
+              <ControlesPage />
+            </PrivateRoute>
+          } />
+          <Route path="/controles/:id" element={
+            <PrivateRoute>
+              <ControleDetail />
+            </PrivateRoute>
+          } />
+          <Route path="/controles/:id/modifier" element={
+            <PrivateRoute>
+              <ControleEdit />
+            </PrivateRoute>
+          } />
+          <Route path="/personnel" element={
+            <PrivateRoute>
+              <Personnel />
+            </PrivateRoute>
+          } />
+          <Route path="/personnel/nouveau" element={
+            <PrivateRoute>
+              <PersonnelForm />
+            </PrivateRoute>
+          } />
+          <Route path="/personnel/:id" element={
+            <PrivateRoute>
+              <PersonnelDetail />
+            </PrivateRoute>
+          } />
+          <Route path="/personnel/:id/equipements" element={
+            <PrivateRoute>
+              <PersonnelEquipements />
+            </PrivateRoute>
+          } />
+          <Route path="/habillement" element={
+            <PrivateRoute>
+              <Habillement />
+            </PrivateRoute>
+          } />
+          <Route path="/notifications" element={
+            <PrivateRoute>
+              <Notifications />
+            </PrivateRoute>
+          } />
+          <Route path="/reports" element={
+            <PrivateRoute>
+              <Rapports />
+            </PrivateRoute>
+          } />
+          <Route path="/settings" element={
+            <PrivateRoute>
+              <Settings />
             </PrivateRoute>
           } />
           <Route path="*" element={<NotFound />} />
