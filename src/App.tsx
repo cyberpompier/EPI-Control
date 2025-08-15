abecf9cfbacd42495e846ad8961aca178c818eaa
cyberpompier <cyberpompier@gmail.com>
2025-08-15 09:39:59 +0200

    Reverted all changes back to version 57bc96839955218962fc082045cea3e4ae93adde

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,48 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Index from './pages/Index';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Dashboard from './pages/Dashboard';
+import Personnel from './pages/Personnel';
+import Equipements from './pages/Equipements';
 import Controles from './pages/Controles';
-import ControleDetails from './pages/ControleDetails';
+import Settings from './pages/Settings';
+import Login from './pages/Login';
+import { SessionProvider } from '@/components/auth/SessionProvider';
+import PrivateRoute from './components/auth/PrivateRoute';
+import PersonnelDetail from './pages/PersonnelDetail';
+import PersonnelForm from './pages/PersonnelForm';
+import EquipementForm from './pages/EquipementForm';
+import PersonnelEquipements from './pages/PersonnelEquipements';
+import EquipementDetail from './pages/EquipementDetail';
+import Reports from './pages/Reports';
+import Notifications from './pages/Notifications';
+import Profile from './pages/Profile';
+import ControleForm from './pages/ControleForm';
+import ControleDetail from './pages/ControleDetail';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Index />} />
-        <Route path="/controles" element={<Controles />} />
-        <Route path="/controles/:id" element={<ControleDetails />} />
-      </Routes>
-    </BrowserRouter>
+    <Router>
+      <SessionProvider>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
+          <Route path="/dashboard" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
+          <Route path="/personnel" element={<PrivateRoute><Personnel /></PrivateRoute>} />
+          <Route path="/personnel/nouveau" element={<PrivateRoute><PersonnelForm /></PrivateRoute>} />
+          <Route path="/personnel/:id" element={<PrivateRoute><PersonnelDetail /></PrivateRoute>} />
+          <Route path="/personnel/:id/equipements" element={<PrivateRoute><PersonnelEquipements /></PrivateRoute>} />
+          <Route path="/equipements" element={<PrivateRoute><Equipements /></PrivateRoute>} />
+          <Route path="/equipements/nouveau" element={<PrivateRoute><EquipementForm /></PrivateRoute>} />
+          <Route path="/equipements/:id" element={<PrivateRoute><EquipementDetail /></PrivateRoute>} />
+          <Route path="/controle/:id" element={<PrivateRoute><ControleForm /></PrivateRoute>} />
+          <Route path="/controles" element={<PrivateRoute><Controles /></PrivateRoute>} />
+          <Route path="/controles/:id" element={<PrivateRoute><ControleDetail /></PrivateRoute>} />
+          <Route path="/parametres" element={<PrivateRoute><Settings /></PrivateRoute>} />
+          <Route path="/reports" element={<PrivateRoute><Reports /></PrivateRoute>} />
+          <Route path="/notifications" element={<PrivateRoute><Notifications /></PrivateRoute>} />
+          <Route path="/profile" element={<PrivateRoute><Profile /></PrivateRoute>} />
+        </Routes>
+      </SessionProvider>
+    </Router>
   );
 }
 
