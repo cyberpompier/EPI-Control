0b8122809abf8606b617fb0da8096ef0911489e7
cyberpompier <cyberpompier@gmail.com>
2025-08-25 13:46:09 +0200

    Reverted all changes back to version 705c01944dbd42b601c21d455417d9df7727fb0a

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-86c9b217'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.ik8lu5teqmg"
+    "revision": "0.vs4ebbdb2l"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
