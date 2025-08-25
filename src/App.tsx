c33a759f021ac4f2cd9a77145b94855a2e50c955
cyberpompier <cyberpompier@gmail.com>
2025-08-25 15:45:21 +0200

    [dyad] Restauration des routes de l'application - wrote 2 file(s) + extra files edited outside of Dyad

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 "use client";
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Reports from './pages/Reports';
+import Index from './pages/Index';
+import Login from './pages/Login';
+import Personnel from './pages/Personnel';
+import Equipements from './pages/Equipements';
+import Controles from './pages/Controles';
+import Rapports from './pages/Rapports';
 import Notifications from './pages/Notifications';
 import PrivateRoute from './components/auth/PrivateRoute';
 
@@ -9,9 +14,13 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/reports" element={<PrivateRoute><Reports /></PrivateRoute>} />
+        <Route path="/" element={<Index />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/personnel" element={<PrivateRoute><Personnel /></PrivateRoute>} />
+        <Route path="/equipements" element={<PrivateRoute><Equipements /></PrivateRoute>} />
+        <Route path="/controles" element={<PrivateRoute><Controles /></PrivateRoute>} />
+        <Route path="/rapports" element={<PrivateRoute><Rapports /></PrivateRoute>} />
         <Route path="/notifications" element={<PrivateRoute><Notifications /></PrivateRoute>} />
-        {/* Autres routes */}
       </Routes>
     </Router>
   );
