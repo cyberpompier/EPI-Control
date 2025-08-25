25620fdc4c6476a9bbdd5b4836e2f41b50654d83
cyberpompier <cyberpompier@gmail.com>
2025-08-25 13:27:05 +0200

    [dyad] wrote 3 file(s) + extra files edited outside of Dyad

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-86c9b217'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.epe0g351he"
+    "revision": "0.ik8lu5teqmg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
