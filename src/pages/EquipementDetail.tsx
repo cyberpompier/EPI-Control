7c2b1f5a0226982502d53820ae10211e5d877490
cyberpompier <cyberpompier@gmail.com>
2025-08-26 09:28:17 +0200

    Updated src/pages/EquipementDetail.tsx

--- src/pages/EquipementDetail.tsx
+++ src/pages/EquipementDetail.tsx
@@ -75,6 +75,7 @@ const EquipementDetail = () => {
         <div className="text-2xl font-bold text-gray-800 mb-4">Équipement non trouvé</div>
         <Button onClick={() => navigate('/equipements')}>Retour à la liste</Button>
       </div>
+      </Layout>
     );
   }
 
