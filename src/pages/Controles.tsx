151acee944e038565559871dab9e5fc8935eba82
cyberpompier <cyberpompier@gmail.com>
2025-10-24 09:36:28 +0200

    Updated src/pages/Controles.tsx

--- src/pages/Controles.tsx
+++ src/pages/Controles.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useMemo, useState } from 'react';
 import { Card, CardContent, CardHeader, CardTitle } from '../components/ui/card';
+import Layout from '@/components/layout/Layout';
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '../components/ui/table';
 import { Badge } from '../components/ui/badge';
 import { supabase } from '../integrations/supabase/client';
